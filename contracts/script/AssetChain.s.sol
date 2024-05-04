89ad0c49e1c8a66cb3ea1fd91ebc06553e7d5b4b
Lamsy <olamide.adetula.1744@gmail.com>
2024-05-04 15:40:28 +0100

    ⌛ update deployment script and contract

--- contracts/script/AssetChain.s.sol
+++ contracts/script/AssetChain.s.sol
@@ -3,15 +3,20 @@ pragma solidity ^0.8.13;
 
 import {Script, console} from "forge-std/Script.sol";
 import {AssetVerification} from "../src/AssetVerification.sol";
+import {AssetFactory} from "../src/AssetFactory.sol";
+import {AssetFractionaliser} from "../src/AssetFractionaliser.sol";
 
 contract AssetChainScript is Script {
     AssetVerification asset;
+    AssetFactory assetFactory;
+    AssetFractionaliser assetFractionaliser;
 
     address owner = 0x2a82addEEbf70F06e56EceF51Abd6b1927A0A39E;
 
     function run() public {
         vm.startBroadcast();
         asset = new AssetVerification();
+        assetFactory = new AssetFactory(address(asset));
 
         vm.stopBroadcast();
     }
--- contracts/src/AssetVerification.sol
+++ contracts/src/AssetVerification.sol
@@ -2,7 +2,6 @@
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
-// import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
